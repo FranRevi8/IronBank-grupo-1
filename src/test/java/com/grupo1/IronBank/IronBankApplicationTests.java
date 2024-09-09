cff76cf9a1efa37d57925339ca3689943830440f
FranRevi8 <fcorevidiego@gmail.com>
2024-09-09 10:33:41 +0200

    first commit

---
+++ src/test/java/com/grupo1/IronBank/IronBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo1.IronBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
