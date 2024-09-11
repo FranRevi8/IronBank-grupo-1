d9e2e823fba4f06d627eed6f8e2275acf4917cd8
FranRevi8 <fcorevidiego@gmail.com>
2024-09-11 11:37:13 +0200

    added CRUD to AccountHolder

--- src/main/java/com/grupo1/IronBank/controller/AccountHolderController.java
+++ src/main/java/com/grupo1/IronBank/controller/AccountHolderController.java
@@ -1,9 +1,13 @@
 package com.grupo1.IronBank.controller;
 
+import com.grupo1.IronBank.model.AccountHolder;
 import com.grupo1.IronBank.service.AccountHolderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("account-holder")
@@ -11,4 +15,32 @@ public class AccountHolderController {
 
     @Autowired
     private AccountHolderService accountHolderService;
+
+    @GetMapping
+    public List<AccountHolder> getAllAccountHolders(){return accountHolderService.getAllAccountHolders();}
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountHolder> getAccountHolderById(@PathVariable Long id){
+        Optional<AccountHolder> accountHolder = accountHolderService.getAccountHolderById(id);
+        return accountHolder.map(ResponseEntity::ok).orElseGet(()-> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public AccountHolder createAccountHolder(@RequestBody AccountHolder accountHolder){
+        return accountHolderService.createAccountHolder(accountHolder);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAccountHolder(@PathVariable Long id){
+        if(accountHolderService.deleteAccountHolder(id)){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AccountHolder> updateAccountHolder(@PathVariable Long id, @RequestBody AccountHolder accountHolder){
+        Optional<AccountHolder> updatedAccountHolder = accountHolderService.updateAccountHolder(id, accountHolder);
+        return updatedAccountHolder.map(ResponseEntity::ok).orElseGet(()-> ResponseEntity.notFound().build());
+    }
 }
--- src/main/java/com/grupo1/IronBank/service/AccountHolderService.java
+++ src/main/java/com/grupo1/IronBank/service/AccountHolderService.java
@@ -1,12 +1,42 @@
 package com.grupo1.IronBank.service;
 
+import com.grupo1.IronBank.model.AccountHolder;
 import com.grupo1.IronBank.repository.AccountHolderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AccountHolderService {
 
     @Autowired
     private AccountHolderRepository accountHolderRepository;
+
+    public List<AccountHolder> getAllAccountHolders(){return accountHolderRepository.findAll();}
+
+    public Optional<AccountHolder> getAccountHolderById(Long id){
+        return accountHolderRepository.findById(id);
+    }
+
+    public AccountHolder createAccountHolder(AccountHolder accountHolder){
+        return accountHolderRepository.save(accountHolder);
+    }
+
+    public boolean deleteAccountHolder(Long id){
+        if(accountHolderRepository.existsById(id)){
+            accountHolderRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    public Optional<AccountHolder> updateAccountHolder(Long id, AccountHolder accountHolder){
+        if(accountHolderRepository.existsById(id)){
+            accountHolder.setId(id);
+            return Optional.of(accountHolderRepository.save(accountHolder));
+        }
+        return Optional.empty();
+    }
 }
